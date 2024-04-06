38552a08997d2d0e8ba83332d5ae9d33550bb199
Prasan <prasanforhire@gmail.com>
2024-04-06 15:34:48 -0400

    feat: iOS Camera

--- composeApp/src/iosMain/kotlin/com.prasan.qrscanner/presentation/CameraPreview.ios.kt
+++ composeApp/src/iosMain/kotlin/com.prasan.qrscanner/presentation/CameraPreview.ios.kt
@@ -1,19 +1,78 @@
 package com.prasan.qrscanner.presentation
 
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.interop.UIKitView
 import androidx.compose.ui.unit.dp
 import kotlinx.cinterop.ExperimentalForeignApi
+import platform.AVFoundation.*
 import platform.UIKit.UIView
 
-@OptIn(ExperimentalForeignApi::class)
 @Composable
-actual fun CameraPreview(visible: Boolean, onCameraError: () -> Unit,  onCodeScanner: (String) -> Unit) {
-    /*val cameraPreview: UIView = createCameraPreview()
-    UIKitView(factory = { cameraPreview }, modifier = Modifier.size(300.dp),)*/
+actual fun CameraPreview(visible: Boolean, onCameraError: () -> Unit, onCodeScanner: (String) -> Unit) {
+    Text("Camera Preview")
+    Spacer(modifier = Modifier.height(16.dp))
+    var previewLayer by remember { mutableStateOf<AVCaptureVideoPreviewLayer?>(null) }
 
-    Text("Not available on iOS")
+    LaunchedEffect(key1 = Unit) {
+
+        checkCameraPermission {
+            if (!it) {
+                onCameraError()
+            } else {
+                setupCameraPreview { layer ->
+                    previewLayer = layer
+                }
+            }
+        }
+    }
+
+    previewLayer?.let { layer ->
+        DisposableEffect(key1 = layer) {
+            val view = UIView().apply { this.layer.addSublayer(layer) }
+            onDispose {
+                view.layer.removeFromSuperlayer()
+            }
+        }
+    }
+}
+
+private fun checkCameraPermission(callback: (Boolean) -> Unit) {
+    val authorizationStatus = AVCaptureDevice.authorizationStatusForMediaType(AVMediaTypeVideo)
+    when (authorizationStatus) {
+        AVAuthorizationStatusAuthorized -> callback(true)
+        AVAuthorizationStatusNotDetermined -> {
+            AVCaptureDevice.requestAccessForMediaType(AVMediaTypeVideo) { granted ->
+                // Handle permission request result (update state or UI if needed)
+                callback(granted)
+            }
+        }
+        else -> callback(false)
+    }
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private fun setupCameraPreview(completion: (AVCaptureVideoPreviewLayer) -> Unit) {
+    val captureSession = AVCaptureSession()
+    val device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+
+    device?.let {
+        val input = try {
+            AVCaptureDeviceInput(device = it, error = null)
+        } catch (e: Exception) {
+            return
+        }
+        if (captureSession.canAddInput(input)) {
+            captureSession.addInput(input)
+        }
+
+        val previewLayer = AVCaptureVideoPreviewLayer(session = captureSession).apply {
+            videoGravity = AVLayerVideoGravityResizeAspectFill
+        }
+        completion(previewLayer)
+
+        captureSession.startRunning()
+    }
 }
